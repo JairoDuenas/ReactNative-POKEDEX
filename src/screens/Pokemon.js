cd85f34d8ac98012bc2856739d445507a11aa29c
JairoDuenas <jairoduenas.ing@gmail.com>
2022-06-16 17:59:01 -0500

    Consultando la API de detalles del Pokemon

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,11 +1,32 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getPokemonApi, getPokemonDetailApi } from "../api/pokemon";
 
 export default function Pokemon(props) {
-  const { navigation, route } = props;
+  const {
+    navigation,
+    route: { params },
+  } = props;
+  const [pokemon, setPokemon] = useState(null);
+  console.log(params.id);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getPokemonDetailApi(params.id);
+        setPokemon(response);
+      } catch (error) {
+        navigation.goBack();
+      }
+    })();
+  }, [params]);
+
+  if (!pokemon) return null;
+
   return (
     <View>
       <Text>Estamos en un Pokemon</Text>
+      <Text>{pokemon.name}</Text>
     </View>
   );
 }
