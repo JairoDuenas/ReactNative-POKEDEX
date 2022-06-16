316e4f0aa2cc2c2ad69e1f4b89701d6155937c97
JairoDuenas <jairoduenas.ing@gmail.com>
2022-06-15 20:43:34 -0500

    Configuración de React Navigation

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import Navigation from "./src/navigation/Navigation";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Estamos en el POKEDEX</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
