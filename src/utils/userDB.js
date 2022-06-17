367a0d2a4309d435cfda08ff6434e5b1b134a4d0
JairoDuenas <jairoduenas.ing@gmail.com>
2022-06-17 11:03:28 -0500

    ✨ Pantalla de Mi cuenta

---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: "jairo",
+  password: "123456",
+};
+
+export const userDetails = {
+  username: "jairo",
+  firstName: "Jairo",
+  lastName: "Duenas",
+  email: "jairo_251180@hotmail.com",
+};
