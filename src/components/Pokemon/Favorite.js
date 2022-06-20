add1f5219bd2bd6b115dab66c4e0bdd80fe4d064
JairoDuenas <jairoduenas.ing@gmail.com>
2022-06-20 16:41:45 -0500

    Almacenado un Pokemón favorito a AsyncStorage

--- src/components/Pokemon/Favorite.js
+++ src/components/Pokemon/Favorite.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Icon from "react-native-vector-icons/FontAwesome5";
+import { addPokemonFavoriteApi } from "../../api/favorite";
 
 export default function Favorite(props) {
   const { id } = props;
 
-  const addFavorite = () => {
-    console.log("añadir a favoritos", id);
+  const addFavorite = async () => {
+    await addPokemonFavoriteApi(id);
   };
+
   return (
     <Icon
       name="heart"
